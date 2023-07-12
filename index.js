b9b658e1616d2daf8078e5d1f2516fc04ffe6373
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-07-12 23:49:54 +0100

    text below bar changes with status

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const MEDIUM = 11;
 
 const passwordInput = document.getElementById('password-type');
 const progressBar = document.getElementById('password-progress');
+const progressLabel = document.getElementById('password-label');
 
 progressBar.value = 0;
-progressBar.className = 'progress progress--low';
+progressBar.className = 'progress__bar progress__bar--low';
+progressLabel.className = 'progress__label progress__label--low';
 
 progressBar.max = MAX_PASSWORD_VALUE;
 passwordInput.maxLength = MAX_PASSWORD_LENGTH;
@@ -18,12 +20,17 @@ function updateProgressBar (event) {
   const inputLength = passwordInput.value.length + (event.key === "Backspace" ? -1 : 1);
   progressBar.value = (MAX_PASSWORD_VALUE / MAX_PASSWORD_LENGTH) * inputLength;
 
-  if (inputLength <= LOW)
-    progressBar.className = 'progress progress--low';
-  else if (inputLength <= MEDIUM)
-    progressBar.className = 'progress progress--medium';
-  else
-    progressBar.className = 'progress progress--high';
-
-  console.log(progressBar.className);
+  if (inputLength <= LOW) {
+    progressBar.className = 'progress__bar progress__bar--low';
+    progressLabel.className = 'progress__label progress__label--low';
+    progressLabel.textContent = 'Should be longer';
+  } else if (inputLength <= MEDIUM) {
+    progressBar.className = 'progress__bar progress__bar--medium';
+    progressLabel.className = 'progress__label progress__label--medium';
+    progressLabel.textContent = 'Pretty good';
+  } else {
+    progressBar.className = 'progress__bar progress__bar--high';
+    progressLabel.className = 'progress__label progress__label--high';
+    progressLabel.textContent = 'Grrrreat!';
+  }
 }
