abf14a96998917dbb55eaa1f42d75ceb8b160f3d
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-07-12 21:58:00 +0100

    progress bar's initial value is zero

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const MAX_PASSWORD_VALUE = 100;
 const passwordInput = document.getElementById('password-type');
 const progressBar = document.getElementById('password-progress');
 
+progressBar.value = 0;
+
 passwordInput.addEventListener('keypress', () => {
   const inputLength = passwordInput.value.length + 1;
   progressBar.value = 5 * inputLength;
